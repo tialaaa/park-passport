b8caf000d42e415ab220b59b855228707eeebf99
Tiala Young <tialayoung@gmail.com>
2023-06-06 16:09:21 -0600

    Refactor App to class comp, write componentDidMount using imported getData

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,49 @@
 import logo from '../logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { getData } from '../apiCalls';
 
-function App() {
-  return (
-    <div className="App">
-      {console.log(process.env.REACT_APP_API_KEY)}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      parks: [],
+      userVisited: [],
+      error: '',
+    }
+  };
+
+  componentDidMount = () => {
+    getData()
+      .then(res => {
+        this.setState({ parks: res.data })
+        console.log(this.state)
+      })
+      .catch(err => this.setState({ error: err }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* {console.log(process.env.REACT_APP_API_KEY)} */}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
