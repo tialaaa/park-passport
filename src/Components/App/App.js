6bf6b6c043cf4dd1b77dc5874cb59378c9db3deb
Tiala Young <tialayoung@gmail.com>
2023-06-07 09:44:12 -0600

    Add component directories and files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,8 @@
-import logo from '../logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { getData } from '../apiCalls';
-import { cleanData } from '../utilities';
+import { getData } from '../../apiCalls';
+import { cleanData } from '../../utilities';
 
 class App extends Component {
   constructor() {
@@ -28,20 +27,7 @@ class App extends Component {
     return (
       <div className="App">
         {/* {console.log(process.env.REACT_APP_API_KEY)} */}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">Park Passport</header>
       </div>
     );
   };
