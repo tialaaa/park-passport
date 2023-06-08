39a7cc849e7ef1b579969ab3d191c582e9e3e28e
Tiala Young <tialayoung@gmail.com>
2023-06-07 19:05:00 -0600

    Add Navlink back to home

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,13 @@
 import './Header.css';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <header className='header'>
-      <h1>Park Passport</h1>
+      <NavLink to='/' style={{ color: 'inherit', textDecoration: 'inherit'}}>
+        <h1>Park Passport</h1>
+      </NavLink>
       {/* TO DO: Add a logo */}
     </header>
   )
