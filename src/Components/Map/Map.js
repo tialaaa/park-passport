ac4d92fbd0bddd41c7ac8e3454c743739bc4c93e
Tiala Young <tialayoung@gmail.com>
2023-06-07 18:29:06 -0600

    Add iframe title and refactor

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
 export const Map = ({ parkName }) => {
-  const mapSource = `https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_API_KEY}&q=${parkName}&maptype=satellite`;
-
   return (
     <iframe
+      title={`Google Maps View of ${parkName}`}
       width="600"
       height="450"
       style={{border:0}}
       loading="lazy"
       referrerPolicy="no-referrer-when-downgrade"
-      src={mapSource}>
+      src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_API_KEY}&q=${parkName}&maptype=satellite`}>
     </iframe>
   )
 }
