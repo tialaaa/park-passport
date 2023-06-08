dced6d2cb7b52cc705837e3a1f85c221dc9477ed
Tiala Young <tialayoung@gmail.com>
2023-06-08 14:11:34 -0600

    Finish Message component and add basic styling

--- src/Components/Message/Message.js
+++ src/Components/Message/Message.js
@@ -2,19 +2,24 @@ import './Message.css';
 import React from 'react';
 
 export const Message = ({ loadingState, percentVisited }) => {
-  const displayedPercent = Math.ceil(percentVisited() * 100);
-  // const loading = <p>Park information loading...</p>;
-
   let displayedMessage;
+  const loading = "Park information loading...";
+
+  const helper = <>How many US National Parks have you visited?<br/><br/>
+  Browse the list below. Check off the parks you have visited.<br/>
+  Find resources to help plan your future adventures.</>;
+
+  const percent = Math.ceil(percentVisited() * 100);
+  const displayedPercent = <>You have visited {percent}% of the National Parks</>;
 
   if (loadingState) {
-    displayedMessage = <p>Park information loading...</p>;
+    displayedMessage = <p className='message-loading'>{loading}</p>;
   } else {
-    displayedMessage = <p>How many US National Parks have you visited?<br/><br/>
-    Browse the list below. Check off the parks you have visited.<br/>
-    Find resources to help plan your future adventures.<br/><br/>
-    You have visited {displayedPercent}% of the National Parks!</p>
-  }
+    displayedMessage = <>
+      <p className='message-helper'>{helper}</p>
+      <p className='message-percent'>{displayedPercent}</p>
+    </>;
+  };
 
   return (
     <div className='homepage-message'>
