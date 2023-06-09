d2f2bfb741b60c0b814bf9565bce8cceec612221
Tiala Young <tialayoung@gmail.com>
2023-06-09 17:10:40 -0600

    Write two tests and pseudocode the rest

--- cypress/e2e/details-page.cy.js
+++ cypress/e2e/details-page.cy.js
@@ -17,20 +17,37 @@ describe('template spec', () => {
     cy.visit('http://localhost:3000/')
   });
 
-  it('', () => {
-    
+  it('Can click a park name to take you to a details page about that park', () => {
+    cy.get('.link-to-details').last().click()
+    cy.url().should('eq', 'http://localhost:3000/park-details/arch')
+    cy.get('.banner-image').should('have.attr', 'src', 'https://www.nps.gov/common/uploads/structured_data/473F5463-F0D2-261D-CEF5FCB39363590B.jpg')
+      .should('have.attr', 'alt', 'A crowd of people sit and watch the sunset at delicate arch.')
+    cy.get('.banner-name').should('have.text', 'Arches National Park')
+    cy.get('.details-info').children().should('have.length', 7)
+    cy.get('.details-info').children().first().should('include.text', 'States: UT')
+      .next().should('include.text', 'Primary Address')
+      .next().should('include.text', 'Discover a landscape of contrasting colors')
+      .next().should('include.text', 'Additional Details from NPS').and('have.attr', 'href')
+    cy.get('iframe').should('have.css', 'width', '600px')
   });
 
-  it('', () => {
-    
+  it('Displays an embedded map with a link to Google Maps', () => {
+    cy.visit('http://localhost:3000/park-details/arch')
+    cy.get('iframe').should('have.attr', 'src', 'https://www.google.com/maps/embed/v1/place?key=AIzaSyBHnR_gWiIkY0g7Cu2bh08YKjDnpa0YYGc&q=Arches National Park&maptype=satellite')
   });
 
-  it('', () => {
-    
+  it('Displays a link to be taken to the NPS website', () => {
+    // visit details page
+    // get link
+    // click
+    // assert new url
   });
 
-  it('', () => {
-    
+  it('Can navigate back to the homepage by clicking the header logo', () => {
+    // visit details page
+    // get header logo
+    // click
+    // assert new url
   });
 
   it('', () => {
