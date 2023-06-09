e8c0297a26dcea328b4cbf5232ad3e0925a6e890
Tiala Young <tialayoung@gmail.com>
2023-06-09 15:23:06 -0600

    Write tests for error handling

--- cypress/e2e/error-handling.cy.js
+++ cypress/e2e/error-handling.cy.js
@@ -1,5 +1,43 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  let npsApiKey = Cypress.env('nps_api_key');
+
+  it('Displays a message for server-side errors when network request is not successful', () => {
+    cy.intercept({
+      pathname: '/api/v1/parks',
+      query: {
+        limit: '469',
+        api_key: npsApiKey,
+      },
+    }, {
+      statusCode: 500,
+      body: {
+        message: 'Internal Server Error'
+      }
+    })
+    cy.visit('http://localhost:3000/')
+
+    cy.get('.logo-link').should('include.text', 'ParkPassport')
+      .get('.logo').should('have.attr', 'src', '/PP-logo.png')
+      .get('.error-message').should('have.text', 'We seem to be having technical issues. Please try again later.')
+  });
+
+  it('Displays a different message for client-side errors when the URL doesn\' exist, and can navigate back to homepage using the Header logo', () => {
+    cy.intercept({
+      pathname: '/api/v1/parks',
+      query: {
+        limit: '469',
+        api_key: npsApiKey,
+      },
+    }, {
+      statusCode: 200,
+      fixture: 'parks'
+    })
+    cy.visit('http://localhost:3000/acadia')
+
+    cy.get('.logo-link').should('include.text', 'ParkPassport')
+      .get('.logo').should('have.attr', 'src', '/PP-logo.png')
+      .get('.error-message').should('have.text', 'Page not found. Please click the logo above to return home.')
+    cy.get('.logo-link').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
+});
