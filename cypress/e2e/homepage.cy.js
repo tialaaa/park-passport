80338354ff7c87c9e1496972581a5b52c9eae8e8
Tiala Young <tialayoung@gmail.com>
2023-06-09 12:21:31 -0600

    Write working intercept finally

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,51 +1,51 @@
 describe('Homepage user flows', () => {
-  // let npsApiKey;
+  let npsApiKey;
 
   beforeEach(() => {
-    // npsApiKey = Cypress.env('nps_api_key');
-    // console.log(npsApiKey)
-    // cy.intercept("GET", `https://developer.nps.gov/api/v1/parks?limit=25&api_key=${npsApiKey}`, {
-
-    // cy.intercept("GET", `https://developer.nps.gov/api/v1/parks?limit=25&api_key=GgsaQEMV2S5mPzKNwSbKIay6hBjcNvFC0Be2Gvi0`, {
+    npsApiKey = Cypress.env('nps_api_key');
+    // ALTERNATE METHOD - Use if test bugs keep coming up
+    // cy.intercept("GET", `https://developer.nps.gov/api/v1/parks*`, {
     //   statusCode: 200,
     //   fixture: 'parks'
     // })
-    // cy.visit('http://localhost:3000/')
 
-    cy.intercept('GET', 'https://developer.nps.gov/api/v1/parks?limit=25&api_key=GgsaQEMV2S5mPzKNwSbKIay6hBjcNvFC0Be2Gvi0', {
+    cy.intercept({
+      pathname: '/api/v1/parks',
+      query: {
+        limit: '469',
+        api_key: npsApiKey,
+      },
+    }, {
       statusCode: 200,
       fixture: 'parks'
     })
-    // }).as('getData')
-    // cy.wait('@getData')
     cy.visit('http://localhost:3000/')
-
   })
 
-  it('Should display a header with the site logo and name', () => {
+  it('Display a header with the site logo and name', () => {
     cy.get('.logo-link').should('include.text', 'ParkPassport')
       .get('.logo').should('have.attr', 'src', '/PP-logo.png')
       .get('.logo').should('have.attr', 'alt', 'tan-colored line drawing of wilderness scene and the sun on green backdrop')
     cy.get('.header-percent').should('not.be.visible')
   })
 
-  it('Should', () => {
+  it('', () => {
     
   })
 
-  it('Should', () => {
+  it('', () => {
     
   })
 
-  it('Should', () => {
+  it('', () => {
     
   })
 
-  it('Should', () => {
+  it('', () => {
     
   })
 
-  it('Should', () => {
+  it('', () => {
     
   })
 })
