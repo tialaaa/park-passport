b2c8c72579e38df1f792045bd427c0e8c1b3d7d4
Tiala Young <tialayoung@gmail.com>
2023-06-09 10:36:07 -0600

    Rename NPS api key for clarity

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 function getData() {
-  return fetch(`https://developer.nps.gov/api/v1/parks?limit=469&api_key=${process.env.REACT_APP_API_KEY}`)
+  return fetch(`https://developer.nps.gov/api/v1/parks?limit=469&api_key=${process.env.REACT_APP_NPS_API_KEY}`)
     .then(response => {
       if (!response.ok) {
         throw Error('Server error')
