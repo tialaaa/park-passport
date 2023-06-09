d39ca4bc57d141b096eee5b9742e79f37ac4ca3e
Tiala Young <tialayoung@gmail.com>
2023-06-08 20:45:25 -0600

    Style Header, including clickable logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,10 +5,11 @@ import { NavLink } from 'react-router-dom';
 export const Header = () => {
   return (
     <header className='header'>
-      <NavLink to='/' style={{ color: 'inherit', textDecoration: 'inherit'}}>
-        <h1>Park Passport</h1>
+      <NavLink to='/' className='logo-link' style={{ color: 'inherit', textDecoration: 'inherit'}}>
+        <img className='logo' src='/PP-logo.png'
+          alt='tan-colored line drawing of wilderness scene and the sun on green backdrop'/>
+        <h1>Park<br/>Passport</h1>
       </NavLink>
-      {/* TO DO: Add a logo */}
     </header>
-  )
-}
+  );
+};
